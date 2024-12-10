b4d9c186d33d9b28c6fd1c025bac5f98c87c6531
Your Name Here <your@email.example>
2024-12-10 17:37:06 +0800

    Test

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/news-archive/', // Set the base path for GitHub Pages
+  build: {
+    outDir: 'build', // Default build folder (can be 'dist' if you're using that)
+  },
 })
